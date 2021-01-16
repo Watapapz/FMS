7ef783de9847e4f083e2d795df0cea5e2af1ed1b
nelson froilan escuro <nelson.escuro@techtaxiinc.com>
2021-01-16 19:03:47 +0800

    Revert "initial"
    This reverts commit 979da10f9582f1047b84bfcba1f6790e22d6cb30.

--- aspnet-core/src/FMS.Application/Authorization/Accounts/Dto/RegisterOutput.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FMS.Authorization.Accounts.Dto
-{
-    public class RegisterOutput
-    {
-        public bool CanLogin { get; set; }
-    }
-}
--- aspnet-core/src/FMS.Web.Core/Authentication/External/IExternalAuthConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace FMS.Authentication.External
-{
-    public interface IExternalAuthConfiguration
-    {
-        List<ExternalLoginProviderInfo> Providers { get; }
-    }
-}
